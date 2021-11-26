cfb8f4efa51ad5f908ff68898b31c2c30f66b4a1
Gustin <florian.gustin.business@gmail.com>
2021-11-26 22:41:03 +0100

    init

---
+++ app/src/main/java/com/florian/esgiandroid/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.florian.esgiandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.florian.esgiandroid.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
