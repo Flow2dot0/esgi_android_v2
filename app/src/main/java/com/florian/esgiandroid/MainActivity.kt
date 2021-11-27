36378db2d2bfca38ca0b8ac7c623d0a8640e3a67
Gustin <florian.gustin.business@gmail.com>
2021-11-27 01:32:58 +0100

    feat : add toolbar

--- app/src/main/java/com/florian/esgiandroid/MainActivity.kt
+++ app/src/main/java/com/florian/esgiandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.florian.esgiandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.florian.esgiandroid.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar_drawable))
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
