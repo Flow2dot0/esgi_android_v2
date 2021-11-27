89543b517f2f0b12389546e4f3baa3b1e2d05e56
Gustin <florian.gustin.business@gmail.com>
2021-11-27 15:45:37 +0100

    init

--- app/src/main/java/com/florian/esgiandroid/MainActivity.kt
+++ app/src/main/java/com/florian/esgiandroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.florian.esgiandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.core.content.ContextCompat
 import com.florian.esgiandroid.ui.main.MainFragment
 
@@ -10,12 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar_drawable))
+        supportActionBar?.hide()
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 300)
+
+        //supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar_drawable))
+
+
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
     }
 }
