0340b76789a339c8dd6e5514a971ec6d059725df
Gustin <florian.gustin.business@gmail.com>
2021-12-01 03:41:27 +0100

    feat : button scan

--- app/src/main/java/com/florian/esgiandroid/presentation/organisms/ProductsFragment.kt
+++ app/src/main/java/com/florian/esgiandroid/presentation/organisms/ProductsFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.florian.esgiandroid.R
+import com.florian.esgiandroid.databinding.MainActivityBinding
 import com.florian.esgiandroid.domain.DefaultProduct
+import com.google.android.play.core.internal.t
 
 class ProductsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,9 @@ class ProductsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return attachToRecyclerView(ProductAdapter(arrayOf(DefaultProduct.dummy(), DefaultProduct.dummy2(), DefaultProduct.dummy(), DefaultProduct.dummy2(), DefaultProduct.dummy3(), DefaultProduct.dummy2(), DefaultProduct.dummy(), DefaultProduct.dummy2())), inflater, container, savedInstanceState)
+        val myView = attachToRecyclerView(ProductAdapter(arrayOf(DefaultProduct.dummy(), DefaultProduct.dummy2(), DefaultProduct.dummy(), DefaultProduct.dummy2(), DefaultProduct.dummy3(), DefaultProduct.dummy2(), DefaultProduct.dummy(), DefaultProduct.dummy2())), inflater, container, savedInstanceState)
+        onClickListener(myView)
+        return myView
     }
 
     private fun attachToRecyclerView(adapter : ProductAdapter, inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +34,13 @@ class ProductsFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(v.context)
         recyclerView.adapter = adapter
         return v
+    }
+
+    private fun onClickListener(view : View) {
+        val button = view.findViewById<Button>(R.id.products_start_scan)
+        button.setOnClickListener {
+            // TODO : Intent - 3 - 1
+        }
     }
 
 }
